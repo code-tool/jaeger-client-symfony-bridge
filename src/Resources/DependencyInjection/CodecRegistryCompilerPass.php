e721769970da2a8638a52a89adc8d64732908286
allflame <allflame@syneforge.com>
2017-11-23 18:27:29 +0200

    Added codec registry compiler pass

---
+++ src/Resources/DependencyInjection/CodecRegistryCompilerPass.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace CodeTool\Opentracing\Symfony\Resources\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+class CodecRegistryCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (false === $container->hasDefinition('jaeger.codec.registry')) {
+            throw new \RuntimeException(
+                sprintf('Required service %s is missing from container', 'codec.registry')
+            );
+        }
+
+        $definition = $container->getDefinition('jaeger.codec.registry');
+        foreach ($container->findTaggedServiceIds('jaeger.codec') as $id => $tags) {
+            foreach ($tags as $tag) {
+                if (false === array_key_exists('name', $tag)) {
+                    throw new \RuntimeException(
+                        sprintf('Required tag field %s is missing from definition', 'name')
+                    );
+                }
+                $definition->addMethodCall('offsetSet', [$tag['name'], new Reference($id)]);
+            }
+        }
+    }
+
+}
