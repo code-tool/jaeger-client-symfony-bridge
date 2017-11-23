3ae9edc9eb54dc1d61f9f114cad0c5c8153e6a51
allflame <allflame@syneforge.com>
2017-11-23 17:44:20 +0200

    Initial commit

---
+++ src/Bridge/TracerBridge.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace CodeTool\Opentracing\Symfony\Bridge;
+
+use CodeTool\OpenTracing\Tracer\FlushableInterface;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class TracerBridge implements EventSubscriberInterface
+{
+    private $tracer;
+
+    public function __construct(FlushableInterface $tracer)
+    {
+        $this->tracer = $tracer;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            ConsoleEvents::TERMINATE => [['onTerminate']],
+            KernelEvents::TERMINATE => [['onTerminate']],
+        ];
+    }
+
+    public function onTerminate()
+    {
+        $this->tracer->flush();
+    }
+}
