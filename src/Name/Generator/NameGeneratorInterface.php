6501742f3e3b0927ad4815ad503cab88d6291902
allflame <allflame@syneforge.com>
2018-02-12 13:10:35 +0200

    PHP5 Compliance

--- src/Name/Generator/NameGeneratorInterface.php
+++ src/Name/Generator/NameGeneratorInterface.php
@@ -1,9 +1,10 @@
 <?php
-declare(strict_types=1);
-
 namespace Jaeger\Symfony\Name\Generator;
 
 interface NameGeneratorInterface
 {
-    public function generate() : string;
+    /**
+     * @return string
+     */
+    public function generate();
 }
