36c3385f33d63f48898b509f891c7af3e062751a
allflame <allflame@syneforge.com>
2018-04-06 18:36:52 +0300

    Added more elaborate background handling

---
+++ src/Bridge/BackgroundFinishListener.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Jaeger\Symfony\Bridge;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class BackgroundFinishListener implements EventSubscriberInterface
+{
+    private $handler;
+
+    public function __construct(BackgroundSpanHandler $handler)
+    {
+        $this->handler = $handler;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [KernelEvents::TERMINATE => ['onTerminate', -4096],];
+    }
+
+    public function onTerminate()
+    {
+        $this->handler->finish();
+
+        return $this;
+    }
+}
---
+++ src/Bridge/BackgroundStartListener.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Jaeger\Symfony\Bridge;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\PostResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class BackgroundStartListener implements EventSubscriberInterface
+{
+    private $handler;
+
+    public function __construct(BackgroundSpanHandler $handler)
+    {
+        $this->handler = $handler;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [KernelEvents::TERMINATE => ['onTerminate', 4096],];
+    }
+
+    public function onTerminate(PostResponseEvent $event)
+    {
+        $this->handler->start($event->getRequest());
+
+        return $this;
+    }
+}
