27c7075df80b7a0554b41c8e42fdc78a46a7ca37
tatarenkoigor <tatarenkoigor@gmail.com>
2018-04-10 13:28:38 +0300

    Add symfony bundle

---
+++ src/JaegerBundle.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Jaeger\Symfony;
+
+use Jaeger\Symfony\Resources\DependencyInjection\CodecRegistryCompilerPass;
+use Jaeger\Symfony\Resources\DependencyInjection\ContextExtractorChainCompilerPass;
+use Jaeger\Symfony\Resources\DependencyInjection\DebugExtractorChainCompilerPass;
+use Jaeger\Symfony\Resources\DependencyInjection\JaegerExtension;
+use Jaeger\Symfony\Resources\DependencyInjection\NameGeneratorChainCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JaegerBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container
+            ->addCompilerPass(new CodecRegistryCompilerPass())
+            ->addCompilerPass(new ContextExtractorChainCompilerPass())
+            ->addCompilerPass(new DebugExtractorChainCompilerPass())
+            ->addCompilerPass(new NameGeneratorChainCompilerPass());
+    }
+
+
+    public function getContainerExtension()
+    {
+        return new JaegerExtension();
+    }
+}
