11d4fde3a9d02738fdec1d97da1e72230ee6f8b6
allflame <allflame@syneforge.com>
2019-07-01 10:35:23 +0300

    Ported functionality to 3.4 symfony

--- src/DependencyInjection/CodecRegistryCompilerPass.php
+++ src/DependencyInjection/CodecRegistryCompilerPass.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Jaeger\Symfony\Resources\DependencyInjection;
+namespace Jaeger\Symfony\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
--- src/JaegerBundle.php
+++ src/JaegerBundle.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 namespace Jaeger\Symfony;
 
-use Jaeger\Symfony\Resources\DependencyInjection\CodecRegistryCompilerPass;
-use Jaeger\Symfony\Resources\DependencyInjection\ContextExtractorChainCompilerPass;
-use Jaeger\Symfony\Resources\DependencyInjection\DebugExtractorChainCompilerPass;
-use Jaeger\Symfony\Resources\DependencyInjection\JaegerExtension;
-use Jaeger\Symfony\Resources\DependencyInjection\NameGeneratorChainCompilerPass;
+use Jaeger\Symfony\DependencyInjection\CodecRegistryCompilerPass;
+use Jaeger\Symfony\DependencyInjection\ContextExtractorChainCompilerPass;
+use Jaeger\Symfony\DependencyInjection\DebugExtractorChainCompilerPass;
+use Jaeger\Symfony\DependencyInjection\NameGeneratorChainCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -23,10 +22,5 @@ class JaegerBundle extends Bundle
             ->addCompilerPass(new ContextExtractorChainCompilerPass())
             ->addCompilerPass(new DebugExtractorChainCompilerPass())
             ->addCompilerPass(new NameGeneratorChainCompilerPass());
-    }
-
-    public function getContainerExtension()
-    {
-        return new JaegerExtension();
     }
 }
