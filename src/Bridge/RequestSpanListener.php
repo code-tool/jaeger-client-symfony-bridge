064d7d1c07d7673b23ce5892d8be9224b5bdf001
anahan <anahan@syneforge.com>
2022-04-17 19:58:35 +0300

    Negative priority for RequestSpanListener::onRequest()

--- src/Bridge/RequestSpanListener.php
+++ src/Bridge/RequestSpanListener.php
@@ -37,7 +37,7 @@ class RequestSpanListener implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            RequestEvent::class => ['onRequest', 0],
+            RequestEvent::class => ['onRequest', -1],
             ResponseEvent::class => ['onResponse', -1024],
             ExceptionEvent::class => ['onKernelException', 0],
         ];
